e9843cb6d805ae6518367f81db41a7a410973fb0
tinkyrain <65303982+tinkyrain@users.noreply.github.com>
2021-05-05 19:24:22 +0500

    watch
    web-watch v1.0

---
+++ app.js
@@ -0,0 +1,34 @@
+window.onload = function(){
+    function setTime(){
+        let time = new Date();
+
+        let watch_hours = time.getHours();
+        let watch_min = time.getMinutes();
+        let watch_sec = time.getSeconds();
+
+        if(watch_hours < 10){
+          watch_hours = "0" + watch_hours;
+        }
+
+        if(watch_min < 10){
+          watch_min = "0" + watch_min;
+        }
+
+        if(watch_sec < 10){
+          watch_sec = "0" + watch_sec;
+        }
+
+        //Часы
+        let hour = document.getElementById('hour').innerText = watch_hours;
+        //Минуты
+        let min = document.getElementById('min').innerText = watch_min;
+        //Секунды
+        let sec = document.getElementById('sec').innerText = watch_sec;
+
+        //Дата
+        let day = document.getElementById('day').innerText = time.getDate();
+        let month = document.getElementById('month').innerText = time.getMonth();
+        let year = document.getElementById('year').innerText = time.getFullYear();
+    }
+    setInterval(setTime, 1000);
+}
